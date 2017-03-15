ef899f50c75a9556371d058118e0e4d54cea4744
Chris Umbel <chris@chrisumbel.com>
2017-03-14 22:51:13 -0400

    reads control edges

--- control.c
+++ control.c
@@ -1,14 +1,68 @@
 
 #include "control.h"
+#include <plib/capture.h>
+#include <plib/timers.h>
 
 int control_init() {
+    TRISC1 = 1; 
+    TRISC2 = 1; 
+    
     return 0;
+}
+
+
+int read_ccp2(unsigned char config) {
+    OpenCapture2(config & CAPTURE_INT_ON);    
+
+    OpenTimer1(TIMER_INT_OFF & T1_SOURCE_INT & T1_SOURCE_CCP);
+    PIR2bits.CCP2IF = 0;     
+    
+    while(PIR2bits.CCP2IF == 0);  // Wait for event
+  
+    int d = ReadCapture2();
+
+    PIR2bits.CCP2IF = 0;
+
+    CloseCapture2();
+    CloseTimer1();  
+    
+    return d;
+}
+
+int read_ccp1(unsigned char config) {
+    OpenCapture1(config & CAPTURE_INT_ON & T1_SOURCE_CCP);    
+
+    OpenTimer1(TIMER_INT_OFF & T1_SOURCE_INT);
+    PIR1bits.CCP1IF = 0; 
+    
+    while(PIR1bits.CCP1IF == 0);  // Wait for event
+  
+    int d = ReadCapture1();
+
+    PIR1bits.CCP1IF = 0;
+
+    CloseCapture1();    
+    CloseTimer1();
+    
+    return d;
 }
 
 int control_read_x() {
-    return 0;
+    PIR1bits.CCP1IF = 0; 
+    
+    SetTmrCCPSrc(T1_SOURCE_CCP);
+    int s = read_ccp1(CAP_EVERY_RISE_EDGE);
+    int e = read_ccp1(CAP_EVERY_FALL_EDGE);
+    
+    return s - e;
 }
 
 int control_read_y() {
-    return 0;
+    PIR2bits.CCP2IF = 0; 
+    
+    SetTmrCCPSrc(T1_SOURCE_CCP);
+    int s = read_ccp2(CAP_EVERY_RISE_EDGE);
+    int e = read_ccp2(CAP_EVERY_FALL_EDGE);
+    
+    return s - e;
 }
--- hardware_configuration.h
+++ hardware_configuration.h
@@ -16,7 +16,7 @@ extern "C" {
 #pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
 #pragma config LVP = OFF, BOR = OFF
 #pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF
-
+#pragma config CCP2MX = ON
 #define _XTAL_FREQ 8000000
 
 #ifdef	__cplusplus
