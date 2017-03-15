6fe5903033a052f8c8a0bbf6d9fa5da2ee0d9e11
Chris Umbel <chris@chrisumbel.com>
2017-03-15 13:48:49 -0400

    style

--- control.c
+++ control.c
@@ -11,16 +11,13 @@ int control_init() {
 
 int read_ccp2(unsigned char config) {
     OpenCapture2(config & CAPTURE_INT_ON);    
-
     OpenTimer1(TIMER_INT_OFF & T1_SOURCE_INT & T1_SOURCE_CCP);
     PIR2bits.CCP2IF = 0;     
     
-    while(PIR2bits.CCP2IF == 0);  // Wait for event
-  
+    while(PIR2bits.CCP2IF == 0);  // Wait for event  
     int d = ReadCapture2();
 
     PIR2bits.CCP2IF = 0;
-
     CloseCapture2();
     CloseTimer1();  
     
@@ -29,16 +26,13 @@ int read_ccp2(unsigned char config) {
 
 int read_ccp1(unsigned char config) {
     OpenCapture1(config & CAPTURE_INT_ON & T1_SOURCE_CCP);    
-
     OpenTimer1(TIMER_INT_OFF & T1_SOURCE_INT);
     PIR1bits.CCP1IF = 0; 
     
     while(PIR1bits.CCP1IF == 0);  // Wait for event
-  
     int d = ReadCapture1();
 
     PIR1bits.CCP1IF = 0;
-
     CloseCapture1();    
     CloseTimer1();
     
@@ -46,8 +40,7 @@ int read_ccp1(unsigned char config) {
 }
 
 int control_read_x() {
-    PIR1bits.CCP1IF = 0; 
-    
+    PIR1bits.CCP1IF = 0;     
     SetTmrCCPSrc(T1_SOURCE_CCP);
     read_ccp1(CAP_EVERY_FALL_EDGE); // get starting point
     int s = read_ccp1(CAP_EVERY_RISE_EDGE);
@@ -57,10 +50,9 @@ int control_read_x() {
 }
 
 int control_read_y() {
-    PIR2bits.CCP2IF = 0; 
-    
+    PIR2bits.CCP2IF = 0;     
     SetTmrCCPSrc(T1_SOURCE_CCP);
-    read_ccp2(CAP_EVERY_FALL_EDGE); // get starting point
+    read_ccp2(CAP_EVERY_FALL_EDGE);
     int s = read_ccp2(CAP_EVERY_RISE_EDGE);
     int e = read_ccp2(CAP_EVERY_FALL_EDGE);
     
